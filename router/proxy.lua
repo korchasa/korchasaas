503af0e9e73f1b4e2a31744a5464340969ded028
korchasa <korchasa@gmail.com>
2016-12-05 19:50:31 +0300

    Remove useless code from router. Fix error response format

--- router/proxy.lua
+++ router/proxy.lua
@@ -95,53 +95,6 @@ r:put('/api/v1/jobs_queue/:id', function(params)
   else
     return ok({id = params.id})
   end
-
-end)
-
-r:get('/api/v1/status', function(params)
-  return ok({
-    name =  "Korchagin Stanislav",
-    role = "Team Lead / Tech Lead / CTO",
-    category = "IT",
-    icon = "https://lh3.googleusercontent.com/O6pzfqAmTVKoAjmiqzK-hd5hZ4xTKnyHZoeCUplAT1D2SQZVMovlLOe8uTxJKtlSLO7Bsx0Mw8cc=w665-h1000-no",
-    description = [[
-      Успешно разрабатывал две(или три) социальные сети, пару CRM-систем, одну BPM, пару платежных витрин с генератором, генератор магазинов, несколько поисковых систем для больших порталов, организовывал отдел разработок, строил релиз-циклы и процессы разработки. А, еще один маленький CDN. Переставать программировать не готов.
-
-      - Управление программистами, требованиями и процессами
-      - Много php, немного java, python, lua, go, etc
-      - Design patterns, OOP, TDD, KISS, YAGNI и другие страшные аббревиатуры
-      - Unit-тесты, приемочное тестирование, стресс тестирование, проверка серверов на пылестойкость и ударопрочность
-      - Извращенные архитектуры, многопоточность, межмашинковое взаимодействие, api, события, масштабирование вширь и вглубь, алгоритмы под данные, данные под задачи, очереди, map/reduce, hash tables, нарезка базы кубиками и колечками
-      - Извращенные архитектуры, многопоточность, межмашинковое взаимодействие, алгоритмы под данные, данные под задачи, очереди, map/reduce, hash tables, нарезка базы кубиками и колечками
-      - Данные больше 100Gb и хиты больше 10M
-      ]],
-    location = {
-      city = "Kiev",
-      countryCode = "UA"
-    },
-    links = {
-      {
-        network = "Twitter",
-        username = "korchasa",
-        url = "https://twitter.com/korchasa"
-      },
-      {
-        network = "Github",
-        username = "korchasa",
-        url = "https://github.com/korchasa"
-      }
-    },
-    locales = {
-      {
-        language = "Russian",
-        fluency = "Native speaker"
-      },
-      {
-        language = "English",
-        fluency = "Чтение документации, переписка, называние переменных"
-      }
-    }
-  })
 end)
 
 ngx.header.content_type = 'application/json';
@@ -163,7 +116,7 @@ if 400 > result.status then
 else
   ngx.print(json.encode({
     status = result.status,
-    message = result.error
+    error = result.error
   }))
   ngx.log(ngx.ERR, result.error)
 end
