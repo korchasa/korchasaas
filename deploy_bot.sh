70b026d4d354405ea258598451bdd9033040697b
korchasa <korchasa@gmail.com>
2016-12-06 16:56:25 +0300

    Setup tokens in env variables

--- deploy_bot.sh
+++ deploy_bot.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 set -ex
+
 if [ "$#" -ne 2 ]; then
     echo "Usage: $0 <telegram_token> <api_host>"
+    exit 1
 fi
 
 docker-compose  -f ./docker-compose.yml -f ./docker-compose.dev.yml build bot
 docker tag korchasaas_bot korchasa/korchasaas-bot:latest
 docker push korchasa/korchasaas-bot:latest
+
 ! hyper rm -f korchasaas-bot
 hyper pull korchasa/korchasaas-bot:latest
 hyper run -e "TELEGRAM_TOKEN=$1" -e "API_HOST=$2" -d -p 80 --size=s1 --name korchasaas-bot korchasa/korchasaas-bot
 ! hyper rmi $(hyper images | grep "^<none>" | awk '{print $3}')
+
 hyper logs -f korchasaas-bot
--- deploy_router.sh
+++ deploy_router.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 set -ex
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <airtable_token>"
+    exit 1
+fi
+
 docker-compose -f ./docker-compose.yml -f ./docker-compose.dev.yml build router
 docker tag korchasaas_router korchasa/korchasaas:latest
 docker push korchasa/korchasaas:latest
+
 IP=$(hyper fip detach korchasaas)
 ! hyper rm -f korchasaas
 hyper pull korchasa/korchasaas:latest
-hyper run -d -p 80 --size=s1 --name korchasaas korchasa/korchasaas
+hyper run -e "AIRTABLE_TOKEN=$1" -d -p 80 --size=s1 --name korchasaas korchasa/korchasaas
 hyper fip attach $IP korchasaas
 ! hyper rmi $(hyper images | grep "^<none>" | awk '{print $3}')
+
 hyper logs -f korchasaas
